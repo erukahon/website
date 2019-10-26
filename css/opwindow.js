c2906d7f26a7a966ea104b99de31a7d1397efbc1
mochiga daisuki <hirokitayama@gmail.com>
2019-10-26 17:10:56 +0900

    initial commit

---
+++ css/opwindow.js
@@ -0,0 +1,19 @@
+
+var w1 = window;
+var w2 = window;
+function openWindowTate(url) {
+   if ((w1 == window) || w1.closed) {
+      w1 = open(url, "_blank", "width=413,height=550, scrollbars=1");
+   } else {
+      w1.focus();
+   }
+   return(false);
+}
+function openWindowYoko(url) {
+  if ((w2 == window) || w2.closed) {
+      w2 = open(url, "_blank", "width=500,height=400, scrollbars=1");
+   } else {
+      w2.focus();
+   }
+   return(false);
+}
